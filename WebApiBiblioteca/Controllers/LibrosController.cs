698e09df2a4129cd56e24492c511af8e6a13a959
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-10-14 08:22:07 -0600

    Datos relacionados

--- WebApiBiblioteca/Controllers/LibrosController.cs
+++ WebApiBiblioteca/Controllers/LibrosController.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiBiblioteca.Models;
 
 namespace WebApiBiblioteca.Controllers;
 
+[ApiController]
+[Route("api/libros")]
 public class LibrosController : ControllerBase
 {
     private readonly AppDbContext db;
@@ -11,6 +15,27 @@ public class LibrosController : ControllerBase
         this.db = db;
     }
 
-    // TODO: Obtener los libros registrados
+    // https://localhost:xxxx/api/libros
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Libro>>> Get()
+    {
+        var libros = await db.Libros.ToListAsync();
+        return libros;
+    }
+
     // TODO: Crear nuevos libros
+    [HttpPost]
+    public async Task<ActionResult> Post(Libro libro)
+    {
+        var existeLibro = await db.Libros.AnyAsync(l => l.Id == libro.Id);
+        var existeAutor = await db.Autores.AnyAsync(a => a.Id == libro.AutorId);
+        if (existeLibro || !existeAutor)
+        {
+            return BadRequest("Error: el libro ya existe o el autor no");
+        }
+
+        await db.Libros.AddAsync(libro);
+        await db.SaveChangesAsync();
+        return Ok();
+    }
 }
---
+++ WebApiBiblioteca/Db/queries.sql
@@ -0,0 +1,10 @@
+-- SQLite
+select * from autores;
+
+-- show tables; (mysql)
+pragma table_list;
+
+-- desc libros; (mysql)
+PRAGMA table_info('Libros');
+
+select * from libros;
